075bb1d12549c0b542085e74d6dc852a3abf3134
camel <camel@camels-MacBook-Pro.local>
2018-08-23 00:03:07 +0530

    Adding post increment operator example

---
+++ lectures/003-basic-data-types/post_increment.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  var j int32 = 5
+  var k int32 = 0
+
+  j++
+  k = j
+
+  fmt.Println(j, k)
+}
+
+// Only for C, C++, java programmers
+// k = j++ // Can't do things like k = j++ or A[k++] etc ....
+// ++j GO doesn't support pre increment operator ++i
