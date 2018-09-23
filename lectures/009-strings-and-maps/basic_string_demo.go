ba388fbd23b677db76261bc851c6df14fec97721
camel <camel@camels-MacBook-Pro-2.local>
2018-09-23 22:54:38 +0530

    Added multi language strings demo

---
+++ lectures/009-strings-and-maps/basic_string_demo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+
+  var friends string = "Tom & Jerry"
+  fmt.Println(friends)
+
+  var tamilHelloFriends string = "வணக்கம் நண்பர்களே!"
+  fmt.Println(tamilHelloFriends)
+
+  var koreanHelloFriends string = "안녕 얘들 아."
+  fmt.Println(koreanHelloFriends)
+}
